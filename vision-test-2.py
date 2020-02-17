4ea416523e3a21a05124c18d96c043c44f67e73a
buerge <emily.buergler@gmail.com>
2020-02-16 18:03:27 -0600

    with previous

--- vision-test-2.py
+++ vision-test-2.py
@@ -90,9 +90,9 @@ def process_name(im, names_list, level_list):
         msg = "**[ERROR]** {0}".format(err)
         print(msg)
         return False
-    booboo = re.search(r"2[\]\)] ", text)
+    booboo = re.search(r"[1-5][\]\)] ", text)
     if (bool(booboo)):
-        text = "21 " + text[booboo.end():]
+        text = text[booboo.start()] + "1 " + text[booboo.end():]
     match = re.search(r"[0-9]+ \S", text)
     if (bool(match)):
         #text = re.sub(r'^\W+', '', text)
