dd6f52b111a748e3f8e79b9a93c0e2e1c6e2d90c
buerge <emily.buergler@gmail.com>
2020-07-23 11:45:40 -0500

    replaced vision-test with vision-test-2

--- vision-test.py
+++ vision-test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 #
 # FILENAME: vision-test.py
-# CREATED:	August 17, 2019
-# AUTHOR:	buerge3
+# CREATED:  August 17, 2019
+# AUTHOR:   buerge3
 #
 # A command-line script for testing image-processing functionality indpendentent
 # of sqlite or discord. Expects a screenshot of an STFC roster as the first
@@ -18,7 +18,7 @@ import sys
 
 # MODIFIABLE PARAMTERS
 img_path = sys.argv[1]
-x_percent = 14
+x_percent = 0.12
 
 # -----------------------------------------------------------------------------
 #                                    FUNCTIONS
@@ -26,29 +26,36 @@ x_percent = 14
 # IsImage
 # @return true if the first command-line argument is an image
 def isImage():
-	return img_path.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'))
+    return img_path.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'))
 
 # get_rgb_filter
 # @param im, the STFC roster screenshot to find appropriate filter values for
 # @returns rgb, a three-element list consisting of the rgb values for the filter
 def get_rgb_filter(im):
-	width, height = im.size
-	rgb = [220, 220, 220]
-	for i in range(3):
-		im_rgb = im.crop((0, 0, width, math.floor(height/10)))
-		print("trying r=" + str(rgb[0]), ", g=" + str(rgb[1]) + ", b=" + str(rgb[2]))
-		apply_img_mask(im_rgb, rgb, x_percent)
-		word = pytesseract.image_to_string(im_rgb)
-		#print("I read: " + word)
-		if (bool(re.search(r"MEM", word))):
-			print("found a working filter! I see: " + word)
-			return rgb;
-		else:
-			rgb[0] -= 20
-			rgb[1] -= 20
-			rgb[2] -= 20
-	print("Unable to process screenshot. I only see: " + word)
-	return None
+    width, height = im.size
+    rgb = [220, 220, 220]
+    for i in range(4):
+        im_rgb = im.crop((0, 0, width, math.floor(height/10)))
+        #logging.debug("trying r=" + str(rgb[0]), ", g=" + str(rgb[1]) + ", b=" + str(rgb[2]))
+        apply_img_mask(im_rgb, rgb, 0)
+        try:
+            word = pytesseract.image_to_string(im_rgb)
+        except TesseractError as err:
+            msg = "**[ERROR]** {0}".format(err)
+            print(msg)
+            return None
+
+        print("I read: " + word)
+        if (bool(re.search(r"MEMBERS", word))):
+            print("found a working filter! I see: " + word)
+            return rgb;
+        else:
+            rgb[0] -= 20
+            rgb[1] -= 20
+            rgb[2] -= 20
+    msg = "**[ERROR]** Unable to find a suitable rgb filter";
+    print(msg)
+    return None
 
 # apply_img_mask
 # @param im, the image to apply a mask to
@@ -58,7 +65,7 @@ def get_rgb_filter(im):
 def apply_img_mask(im, rgb, x_percent):
     pixdata = im.load()
     width, height = im.size
-    x_cutoff = math.floor(width / x_percent)
+    x_cutoff = math.floor(width * x_percent)
     for x in range(width):
         for y in range(height):
             r,g,b = im.getpixel((x,y))
@@ -68,59 +75,56 @@ def apply_img_mask(im, rgb, x_percent):
             else:
                 pixdata[x,y] = (0,0,0,0)
 
-# process_image
+
+# process_name
 # @param im, an STFC roster screenshot
 # @param names_list, an empty list to populate with player names
 # @param level_list, an empty list to populate with player levels
 # @return True if success, False if an error occurred
-def process_image(im, names_list, level_list):
-    width, height = im.size
-    im_names = im.crop((0, math.floor(height/10), math.floor(width/2), height))
-    names = pytesseract.image_to_string(im_names)
-    tmp_list = names.replace("|", "").split('\n\n')
-    success = False
-    __flag = False
-    for tmp in tmp_list:
-        if (bool(re.match(r"^[0-9]+ \S", tmp))):
-            lv, name = tmp.split(' ', 1)
-            level_list.append(lv)
-            names_list.append(name)
-            success = True
-        elif (bool(re.match(r"^[0-9]+$", tmp))):
-            level_list.append(tmp)
-            __flag = True
-        elif (__flag):
-        	if (tmp.strip()):
-	            names_list.append(tmp)
-	            success = True
-        else:
-            names_list.append("DELETE_ME")
-            level_list.append(0)
-    if not success:
-        msg = "Unable to process image; cause: did not identify any rows";
+def process_name(im, names_list, level_list):
+    #width, height = im.size
+    #im_names = im.crop((0, math.floor(height/10), math.floor(width/2), height))
+    try:
+        text = pytesseract.image_to_string(im, config='--psm 7')
+    except Error as err:
+        msg = "**[ERROR]** {0}".format(err)
         print(msg)
-    if len(names_list) != len(level_list):
-        msg = "Unable to process image; cause: did not identify exactly one level for each name"
+        return False
+    booboo = re.search(r"[1-5][\]\)l] ", text)
+    if (bool(booboo)):
+        text = text[booboo.start()] + "1 " + text[booboo.end():]
+    match = re.search(r"[0-9]+ \S", text)
+    if (bool(match)):
+        #text = re.sub(r'^\W+', '', text)
+        text = text[match.start():]
+        lv, name = text.split(' ', 1)
+        name = name.replace(" ", "_")
+        name = re.sub(r'^[0-9]+_', '', name)
+        heart_match = re.search(r"[a-zA-Z0-9]", name); # check for extra whitespace created by hearts
+        if (bool(heart_match)):
+            name = name[heart_match.start():] # handle extra whitespace created by hearts
+        level_list.append(lv)
+        names_list.append(name)
+        return True
+    else:
+        #msg = "**[ERROR]** Unable to process image; cause: did not discover any data in the expected format"
+        msg = "**[ERROR]** Unable to process line {}; cause: did not discover data in the expected format".format(text)
         print(msg)
-        print("NAMES:")
-        for name in names_list:
-            print(name)
-        print("LEVELS:")
-        for lv in level_list:
-            print(lv)
-    return True;
+        im.show()
+        return False
+
 
 # check_spelling
 # @param names_list, a list of player names to check the spelling of
 #         using the dictionary file 'STFC_dict.txt'
-def check_spelling(names_list):
+def check_spelling(names_list, mispelled):
     spell = SpellChecker(language=None, case_sensitive=False)
     spell.word_frequency.load_text_file("STFC_dict.txt")
     
     for i in range(len(names_list)):
         if (names_list[i] == "DELETE_ME"):
             continue
-        word = names_list[i].lower()
+        word = names_list[i]
 
         if word in spell:
             names_list[i] = word
@@ -131,10 +135,12 @@ def check_spelling(names_list):
                 print("Corrected '{}' to '{}'".format(word, cor))
                 names_list[i] = cor;
             else:
-                print("Unrecognized player name " + word + " in row " + str(i))
-                print("If this is a new player, please add them to the dictionary by doing '!add <player name>'")
-                names_list[i] = "DELETE_ME"
+                mispelled.append(word)
+                msg = "**[WARNING]** Unrecognized player name {}".format(word)
+                print(msg)
+                names_list[i] = "DELETE_ME" + names_list[i]
                 continue
+
 # store_in_db
 # @param names_list, a list of player names
 # @param lv_list, a list of player levels
@@ -143,10 +149,10 @@ def check_spelling(names_list):
 # this method only displays what values would be entered into the database,
 # and does not store this data in an actual database
 def store_in_db(names_list, lv_list, power_list, team):
-	for i in range(len(names_list)):
-		if i < len(lv_list) and i < len(power_list) and names_list[i] != "DELETE_ME":
-			msg = "Name: " + names_list[i] + ",\tLv: " + lv_list[i] + ",\tPower: " + power_list[i]
-			print(msg)
+    for i in range(len(names_list)):
+        if i < len(lv_list) and i < len(power_list) and names_list[i] != "DELETE_ME":
+            msg = "Name: " + names_list[i] + ",\tLv: " + lv_list[i] + ",\tPower: " + power_list[i]
+            print(msg)
 
 
 # ------------------------------------------------------------------------------
@@ -155,26 +161,53 @@ def store_in_db(names_list, lv_list, power_list, team):
 if not isImage():
     print('Please only submit images. Stopping...')
 else:
-	im = Image.open(img_path).convert("RGB") 
-	pixdata = im.load()
-	width, height = im.size
-	x_cutoff = math.floor(width / x_percent)
-	names_list = []
-	level_list = []
-	rgb = get_rgb_filter(im)
-	if (rgb is not None):
-		apply_img_mask(im, rgb, x_percent)
-		#im.show()
-		if (process_image(im, names_list, level_list)):
-		    check_spelling(names_list)
-		    width, height = im.size
-		    power_list = []
-		    im_power = im.crop((math.floor(width/2), math.floor(height/10), width, height))
-		    power = pytesseract.image_to_string(im_power)
-		    power_list = power.split('\n')
-		    for i in range(len(power_list)):
-		    	power_list[i] = re.sub("[^0-9]", "", power_list[i])
-		    power_list = list(filter(None, power_list))
-		    store_in_db(names_list, level_list, power_list, "TEST")
-		else:
-			im.show()
+
+    mispelled_list = []
+    success_count = 0
+    failed_count = 0
+
+    im = Image.open(img_path).convert("RGB") 
+    pixdata = im.load()
+    width, height = im.size
+    x_cutoff = math.floor(width / x_percent)
+    names_list = []
+    level_list = []
+    exclude = [0] * 7
+    rgb = get_rgb_filter(im)
+    if rgb is None:
+        msg = "**[ERROR]** Unable to process screenshot; cause: failed to determine a suitable rgb filter"
+        print(msg)
+    else:
+        apply_img_mask(im, rgb, x_percent)
+
+        for k in range(7):
+            a = 2 * height / 10
+            b = (( height - a) / 7 ) * k
+            c = (( height - a) / 7 ) * (k + 1)
+            im_names = im.crop((  0, math.floor( a + b ) , math.floor(width/2), math.floor( a + c ) ))
+            #tasks.append(process_image(ctx, im_names, names_list, level_list))
+            if not process_name(im_names, names_list, level_list):
+                exclude[k] = 1
+        #im.show()
+        check_spelling(names_list, mispelled_list)
+        
+        power_list = []
+        im_power = im.crop((math.floor(width/2), math.floor(height/10), width, height))
+
+        try:
+            power = pytesseract.image_to_string(im_power)
+        except TesseractError as err:
+            fail_count += 7;
+            msg = "**[ERROR]** {0}".format(err)
+            print(msg)
+            quit()
+
+        power_list = power.split('\n')
+        for i in range(len(power_list)):
+            power_list[i] = re.sub("[^0-9,]", "", power_list[i])
+        power_list = list(filter(None, power_list))
+        new_power_list = []
+        for i in range(7):
+            if not exclude[i]:
+                new_power_list.append(power_list[i])
+        store_in_db(names_list, level_list, new_power_list, "TEST")
