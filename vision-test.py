8cbc0b191e78a9cd436f4b33e1b179848ea2fe2a
buerge <emily.buergler@gmail.com>
2020-07-25 17:32:52 -0500

    vision test now uses db

--- vision-test.py
+++ vision-test.py
@@ -9,23 +9,75 @@
 # command-line argument.
 # Usage: "python ./vision-test.py <image_file_path>"
 
-from PIL import Image
-import pytesseract
-import math
-from spellchecker import SpellChecker
-import re
-import sys
+import sqlite3                                   # sqlite3        - connects to the database
+from sqlite3 import Error
+from PIL import Image                            # PIL            - loads and preprocesses images
+import pytesseract                               # Tesseract OCR  - converts images to strings
+import math                                      # math           - performs basic math operations such as min/max
+from spellchecker import SpellChecker            # pyspellchecker - corrects player names using the dictionary
+import re                                        # re             - handles regular expressions
+import sys                                       # sys            - parses command line arguments
+import datetime                                  # datetime       - gets the current date and time
 
 # MODIFIABLE PARAMTERS
-img_path = sys.argv[1]
+db_name = "test.db"
 x_percent = 0.12
+
+# -----------------------------------------------------------------------------
+#                        DATABASE CONNECTION SCRIPT
+# -----------------------------------------------------------------------------
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        print("connected to " + db_file);
+        return conn
+    except Error as e:
+        print(e)
+
+    return None
+
+conn = create_connection(db_name)
 
 # -----------------------------------------------------------------------------
 #                                    FUNCTIONS
 # -----------------------------------------------------------------------------
-# IsImage
-# @return true if the first command-line argument is an image
-def isImage():
+# add_player
+# adds a player to the dictionary/alias table if they do not exist, otherwise just
+# sets the alias table entry to active
+# @param name, a player name string
+# @return key of the added player
+def add_player(name):
+    cur = conn.cursor()
+    sql = '''SELECT key FROM alias WHERE name="name"'''
+    print("SQL: " + sql)
+    cur.execute(sql)
+    res = cur.fetchone()
+    if res:
+        sql = '''UPDATE alias SET active=1 WHERE key={}'''.format(res[0])
+        return res[0]
+    # the player does not exist, add them
+    sql = '''SELECT value FROM __state WHERE name="key"'''
+    print("SQL: " + sql)
+    cur.execute(sql)
+    key = cur.fetchone()[0]
+    sql = '''INSERT INTO alias (key, name, date) VALUES ("{}", "{}")'''.format(key, name.lower(), datetime.datetime.now().strftime("%Y-%m-%d"))
+    print("SQL: " + sql)
+    cur.execute(sql)
+    new_key = int(key) + 1
+    sql = '''UPDATE __state SET value={} WHERE name="key"'''.format(new_key)
+    print("SQL: " + sql)
+    cur.execute(sql)
+    return key
+
+# isImage
+# @param img_path, the path to an image
+# @return true if the specified path is an image
+def isImage(img_path):
     return img_path.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'))
 
 # get_rgb_filter
@@ -75,7 +127,6 @@ def apply_img_mask(im, rgb, x_percent):
             else:
                 pixdata[x,y] = (0,0,0,0)
 
-
 # process_name
 # @param im, an STFC roster screenshot
 # @param names_list, an empty list to populate with player names
@@ -113,13 +164,23 @@ def process_name(im, names_list, level_list):
         im.show()
         return False
 
+# load_dictionary
+# @param spell_check, an instance of spell checker
+def load_dictionary(spell_check):
+    cur = conn.cursor()
+    sql = '''SELECT name FROM alias WHERE active=1'''
+    cur.execute(sql)
+    word_list = []
+    for row in cur.fetchall():
+        word_list.append(row[0])
+    spell_check.word_frequency.load_words(word_list)
 
 # check_spelling
+# @param spell, the spell checker to use for spell checking
 # @param names_list, a list of player names to check the spelling of
 #         using the dictionary file 'STFC_dict.txt'
-def check_spelling(names_list, mispelled):
-    spell = SpellChecker(language=None, case_sensitive=False)
-    spell.word_frequency.load_text_file("STFC_dict.txt")
+# @param mispelled, an empty list to populate with mispelled names
+def check_spelling(spell, names_list, mispelled):
     
     for i in range(len(names_list)):
         if (names_list[i] == "DELETE_ME"):
@@ -158,56 +219,64 @@ def store_in_db(names_list, lv_list, power_list, team):
 # ------------------------------------------------------------------------------
 #                                 MAIN SCRIPT
 # ------------------------------------------------------------------------------
-if not isImage():
-    print('Please only submit images. Stopping...')
+if len(sys.argv) < 2:
+    print('Usage: python3 ./vision-test [image-name]')
+elif not conn:
+    print('Failed to connect to database. Stopping...')
 else:
+    spell_checker = SpellChecker(language=None, case_sensitive=False)
+    load_dictionary(spell_checker)
 
     mispelled_list = []
     success_count = 0
     failed_count = 0
 
-    im = Image.open(img_path).convert("RGB") 
-    pixdata = im.load()
-    width, height = im.size
-    x_cutoff = math.floor(width / x_percent)
-    names_list = []
-    level_list = []
-    exclude = [0] * 7
-    rgb = get_rgb_filter(im)
-    if rgb is None:
-        msg = "**[ERROR]** Unable to process screenshot; cause: failed to determine a suitable rgb filter"
-        print(msg)
-    else:
-        apply_img_mask(im, rgb, x_percent)
-
-        for k in range(7):
-            a = 2 * height / 10
-            b = (( height - a) / 7 ) * k
-            c = (( height - a) / 7 ) * (k + 1)
-            im_names = im.crop((  0, math.floor( a + b ) , math.floor(width/2), math.floor( a + c ) ))
-            #tasks.append(process_image(ctx, im_names, names_list, level_list))
-            if not process_name(im_names, names_list, level_list):
-                exclude[k] = 1
-        #im.show()
-        check_spelling(names_list, mispelled_list)
-        
-        power_list = []
-        im_power = im.crop((math.floor(width/2), math.floor(height/10), width, height))
-
-        try:
-            power = pytesseract.image_to_string(im_power)
-        except TesseractError as err:
-            fail_count += 7;
-            msg = "**[ERROR]** {0}".format(err)
+    for img_path in sys.argv[1:]:
+        if not isImage(img_path):
+            print('Please only submit images. Stopping...')
+            break
+        im = Image.open(img_path).convert("RGB")
+        pixdata = im.load()
+        width, height = im.size
+        x_cutoff = math.floor(width / x_percent)
+        names_list = []
+        level_list = []
+        exclude = [0] * 7
+        rgb = get_rgb_filter(im)
+        if rgb is None:
+            msg = "**[ERROR]** Unable to process screenshot; cause: failed to determine a suitable rgb filter"
             print(msg)
-            quit()
+        else:
+            apply_img_mask(im, rgb, x_percent)
 
-        power_list = power.split('\n')
-        for i in range(len(power_list)):
-            power_list[i] = re.sub("[^0-9,]", "", power_list[i])
-        power_list = list(filter(None, power_list))
-        new_power_list = []
-        for i in range(7):
-            if not exclude[i]:
-                new_power_list.append(power_list[i])
-        store_in_db(names_list, level_list, new_power_list, "TEST")
+            for k in range(7):
+                a = 2 * height / 10
+                b = (( height - a) / 7 ) * k
+                c = (( height - a) / 7 ) * (k + 1)
+                im_names = im.crop((  0, math.floor( a + b ) , math.floor(width/2), math.floor( a + c ) ))
+                #tasks.append(process_image(ctx, im_names, names_list, level_list))
+                if not process_name(im_names, names_list, level_list):
+                    exclude[k] = 1
+            #im.show()
+            check_spelling(spell_checker, names_list, mispelled_list)
+
+            power_list = []
+            im_power = im.crop((math.floor(width/2), math.floor(height/10), width, height))
+
+            try:
+                power = pytesseract.image_to_string(im_power)
+            except TesseractError as err:
+                fail_count += 7;
+                msg = "**[ERROR]** {0}".format(err)
+                print(msg)
+                quit()
+
+            power_list = power.split('\n')
+            for i in range(len(power_list)):
+                power_list[i] = re.sub("[^0-9,]", "", power_list[i])
+            power_list = list(filter(None, power_list))
+            new_power_list = []
+            for i in range(7):
+                if not exclude[i]:
+                    new_power_list.append(power_list[i])
+            store_in_db(names_list, level_list, new_power_list, "TEST")
